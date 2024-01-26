1cfaff9843c55ccbd1703c0f4e6066e48f8a75b3
amal5550 <sloumaamal65@gmail.com>
2024-01-26 16:08:57 +0100

    backend

---
+++ backend/data.js
@@ -0,0 +1,56 @@
+
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', // 679px * 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality Product',
+    },
+    {
+    
+      name: 'Adidas Slim Shirt',
+      slug: 'Adidas-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Nike',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality Product',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-Pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality Product',
+    },
+    {
+  
+      name: 'Adidas Fit Pant',
+      slug: 'Adidas-Fit-Pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality Product',
+    },
+  ],
+};
+export default data;
