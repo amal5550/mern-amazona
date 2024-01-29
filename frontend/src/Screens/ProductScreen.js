f015eaa02b1fc6ae8fb94953b9285428e1c04db6
amal5550 <sloumaamal65@gmail.com>
2024-01-29 16:45:29 +0100

    Add Context API and Store Provider

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useReducer } from 'react';
+import {useContext, useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -12,6 +12,7 @@ import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { getError } from '../utils';
+import { Store } from '../Store';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -44,6 +45,14 @@ useEffect(() => {
   };
   fetchData();
 }, [slug]);
+const {// state, 
+  dispatch: ctxDispatch } = useContext(Store);
+const addToCartHandler = () => {
+  ctxDispatch({
+    type: 'CART_ADD_ITEM',
+    payload: { ...product, quantity: 1 },
+  });
+};
 
 return loading ? (
   <LoadingBox />
@@ -106,8 +115,9 @@ return loading ? (
               {product.countInStock > 0 && (
                 <ListGroup.Item>
                   <div className="d-grid">
-                    <Button variant="primary">Add to Cart</Button>
-                  </div>
+                  <Button onClick={addToCartHandler} variant="primary">
+                        Add to Cart
+                      </Button>                  </div>
                 </ListGroup.Item>
               )}
             </ListGroup>
